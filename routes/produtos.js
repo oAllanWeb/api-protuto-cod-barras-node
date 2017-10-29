6a93330687050848289bfa05a94de7a4c9dcabcb
Allan da Silva <allanweb019@gmail.com>
2017-10-29 21:59:18 -0200

    Correção coleta de dados

--- routes/produtos.js
+++ routes/produtos.js
@@ -7,29 +7,57 @@ const cheerio = require('cheerio')
 /* GET users listing. */
 router.get('/:cod', (req, res, next) => {
   if(Number(req.params.cod))
-    request('https://cosmos.bluesoft.com.br/produtos/'+req.params.cod, (error, response, html) => {
+  request('https://cosmos.bluesoft.com.br/produtos/'+req.params.cod, (error, response, html) => {
+    
+    if(!error){
+      const $ = cheerio.load(html);
       
-      if(!error){
-        const $ = cheerio.load(html);
-        
-        const arr =  $('h1.page-header').text().split('\n')
-        const d = $("dd.description").text().split('\n')
+      const arr =  $('h1.page-header').text().split('\n')
+      const v = $("dd.description").text().split('\n').filter((x)=>{ if(x!=""){return x}})
+      const p = $("dt").text().split('\n').filter((x)=>{ if(x!=""){return x}})
+      
+      
+      
+      
+      
+      
+      if(arr[1]){
+        var txt = ''
+        var str = '{'
+          
+        for (var i = 0; i < p.length; i++) {
 
-        
+          switch (p[i]) {
+            case 'País de Registro:':
+              txt += ',"pais":"'+v[i]+'"'
+              break;
+            case 'Categoria (GPC):':
+              txt += ',"categoria":"'+v[i]+'"'
+              break;
+            case 'Marca:':
+              txt += ',"marca":"'+v[i]+'"'
+              break;
+            case 'Fabricante:':
+              txt += ',"fabricante":"'+v[i]+'"'
+              break;
+            case 'Distribuidores:':
+              txt += ',"distribuidores":"'+v[i]+'"'
+              break;
+          
+            
+          }
 
+                 
+        }
 
+        str += txt.substring(1)
+        str+='}'
+        var obj = JSON.parse(str)
 
-
-        if(arr[1]){
-
-          res.send({
-            nome:arr[1],
-            codigo_barras:arr[5],
-            pais:d[1],
-            fabricante:d[4],
-            distribuidor:d[7],
-            marca:d[10]
-          })
+          obj.nome=arr[1]
+          obj.codigo_barras=arr[5]
+                  
+          res.send(obj)
         }else{
           res.send({status:-1, msg:"Código de barras inválido"})
         }
