6211fff8c0ba60bb57906b26957204102069e621
Allan da Silva <allanweb019@gmail.com>
2017-10-29 23:43:27 -0200

    Atualizado imagem do produto

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const request = require('request')
 const cheerio = require('cheerio')
+const  base64Img = require('base64-img');
+
 
 
 /* GET users listing. */
@@ -16,6 +18,10 @@ router.get('/:cod', (req, res, next) => {
       const v = $("dd.description").text().split('\n').filter((x)=>{ if(x!=""){return x}})
       const p = $("dt").text().split('\n').filter((x)=>{ if(x!=""){return x}})
       
+      const img  = $(".product-thumbnail img").attr('src')
+      console.log(img)
+      const temImg = String(img).split("/")
+      
       
       
       
@@ -24,40 +30,55 @@ router.get('/:cod', (req, res, next) => {
       if(arr[1]){
         var txt = ''
         var str = '{'
-          
+        
         for (var i = 0; i < p.length; i++) {
-
+          
           switch (p[i]) {
             case 'País de Registro:':
-              txt += ',"pais":"'+v[i]+'"'
-              break;
+            txt += ',"pais":"'+v[i]+'"'
+            break;
             case 'Categoria (GPC):':
-              txt += ',"categoria":"'+v[i]+'"'
-              break;
+            txt += ',"categoria":"'+v[i]+'"'
+            break;
             case 'Marca:':
-              txt += ',"marca":"'+v[i]+'"'
-              break;
+            txt += ',"marca":"'+v[i]+'"'
+            break;
             case 'Fabricante:':
-              txt += ',"fabricante":"'+v[i]+'"'
-              break;
+            txt += ',"fabricante":"'+v[i]+'"'
+            break;
             case 'Distribuidores:':
-              txt += ',"distribuidores":"'+v[i]+'"'
+            txt += ',"distribuidores":"'+v[i]+'"'
               break;
           
+              
+            }
+            
             
           }
-
-                 
-        }
-
-        str += txt.substring(1)
-        str+='}'
-        var obj = JSON.parse(str)
-
+          
+          
+          str += txt.substring(1)
+          str+='}'
+          var obj = JSON.parse(str)
+          
           obj.nome=arr[1]
           obj.codigo_barras=arr[5]
-                  
-          res.send(obj)
+          
+          if (temImg[1] == "assets" ) {
+          
+            res.send(obj)
+          }else{
+       
+            base64Img.requestBase64(img, function(erro, resu, body) {
+             
+                
+                obj.img = body
+                
+                res.send(obj)
+            
+          })
+          }
+          
         }else{
           res.send({status:-1, msg:"Código de barras inválido"})
         }
